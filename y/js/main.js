4b4cc601ae84c8df267d1335c42ab701debaabae
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2021-05-04 21:17:41 +0530

    css incorporated

--- y/js/main.js
+++ y/js/main.js
@@ -1,16 +1,18 @@
 $(window).on("load",function() {
     $(window).scroll(function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      if( $(window).width()>991) {
       $(".fade").each(function() {
         /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight()-500;
+        var objectBottom = $(this).offset().top + $(this).outerHeight()-900;
         
         /* If the element is completely within bounds of the window, fade it in */
         if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-          if ($(this).css("opacity")==0) {$(this).fadeTo(50,1);}
+          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
         } else { //object goes out of view (scrolling up)
-          if ($(this).css("opacity")==1) {$(this).fadeTo(50,0);}
+          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
         }
       });
+    }
     }).scroll(); //invoke scroll-handler on page-load
   });
